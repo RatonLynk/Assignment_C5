136c1745188251930fdc1fa975d2f74842b18e75
kieuthibinh03 <kbinh780@gmail.com>
2022-06-17 20:08:11 +0700

    forgot, changes passwword
    ok

---
+++ Assignment/Models/UserChangePasswordVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.Models
+{
+    public class UserChangePasswordVM
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Vui lòng không bỏ trống")]
+        public string NewPassword { get; set; }
+        [Compare("NewPassword", ErrorMessage = "Xác nhận mật khẩu không khớp")]
+        public string ConfirmPassword { get; set; }
+    }
+}
